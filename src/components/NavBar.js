d66bed207aa17d2bfb3fa416d68062f38f2c861e
limlei2 <87996616+limlei2@users.noreply.github.com>
2024-07-21 01:31:20 -0400

    css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,4 @@
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { useState, useEffect } from "react";
 
 export const NavBar = () => {
@@ -37,16 +33,12 @@ export const NavBar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink ==='skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink ==='projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink ==='home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>01. Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink ==='skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>02. Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink ==='projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>03. Projects</Nav.Link>
+            <Nav.Link href="#github" className={activeLink ==='github' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('github')}>04. Github</Nav.Link>
           </Nav>
           <span className="navbar-text">
-            <div className="social-icons">
-                <a href="#"><img src={''} alt=""/></a>
-                <a href="#"><img src={''} alt=""/></a>
-                <a href="#"><img src={''} alt=""/></a>
-            </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
         </Navbar.Collapse>
